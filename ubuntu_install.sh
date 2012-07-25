8f9714c439de468887b93294cdefb6d437aa6859
Tom Cammann <tom.cammann@vmci.net>
2012-07-25 15:34:00 +0100

    minor fixes to install

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -4,11 +4,11 @@
 
 #Check for git
 command git >/dev/null 2>&1
-if [ $? != 0 ]; then
+if [ $? != 1 ]; then
 	echo "Git not found, install git:"
 	sudo apt-get install git
 	command git >/dev/null 2>&1
-	if [ $? != 0 ]; then
+	if [ $? != 1 ]; then
 		echo "Git not found. Exiting."
 		exit 1
 	fi
@@ -25,7 +25,7 @@ mkdir -p ~/.vim/tmp
 mkdir -p ~/.vim/backup
 mkdir -p ~/.vim/undo
 #Copy vim files
-cp -r ~/.dots/vim ~/.vim
+cp -r ~/.dots/vim/* ~/.vim
 cp ~/.dots/vimrc ~/.vimrc
 
 cp ~/.dots/bash_aliases ~/.bash_aliases
@@ -36,11 +36,11 @@ wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master
 git clone git://github.com/rupa/z.git ~/.dots/z
 
 echo "source ~/.bash_aliases" >> ~/.zshrc
-echo ". ~/.dot/z/z.sh" >> ~/.zshrc
+echo ". ~/.dots/z/z.sh" >> ~/.zshrc
 echo "function precmd () {" >> ~/.zshrc
-echo " _z --add \"$(pwd -P)\"" >> ~/.zshrc
+echo " _z --add \"\$(pwd -P)\"" >> ~/.zshrc
 echo "}" >> ~/.zshrc
 
-echo ". ~/.dot/z/z.sh" >> ~/.bashrc
+echo ". ~/.dots/z/z.sh" >> ~/.bashrc
 
 sed -i 's/robbyrussell/dstufft/g' ~/.zshrc
